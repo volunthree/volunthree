4e13462744c9804f3b972f59e4a9dfeee966ee46
Kenji Sanehira <kenjisan@hawaii.edu>
2024-01-22 11:58:41 -1000

    Create the NavBar
    Still need to change the text to light.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
-import { Roles } from 'meteor/alanning:roles';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { BoxArrowRight, CloudDownload, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
-import { ROLE } from '../../api/role/Role';
+import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
 const NavBar = () => {
@@ -13,24 +11,31 @@ const NavBar = () => {
   const { currentUser } = useTracker(() => ({
     currentUser: Meteor.user() ? Meteor.user().username : '',
   }), []);
-  const menuStyle = { marginBottom: '10px' };
+  const menuStyle = { color: 'white' };
+  // TODO: Add links to the respective pages of the NavBar.
   return (
-    <Navbar bg="light" expand="lg" style={menuStyle}>
+    <Navbar bg="primary" expand="lg" style={menuStyle}>
       <Container>
-        <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/"><h1>meteor-application-template-production</h1></Navbar.Brand>
+        <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/">
+          <img
+            alt="Voluntree Logo"
+            src="/images/The_Voluntree_Image.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          TheVoluntree
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
         <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
           <Nav className="me-auto justify-content-start">
-            {currentUser ? ([
-              <Nav.Link id={COMPONENT_IDS.NAVBAR_ADD_STUFF} as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
-              <Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
-            ]) : ''}
-            {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
-              [<Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} to="/admin" key="admin">Admin</Nav.Link>,
-                <NavDropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} title="Manage" key="manage-dropdown">
-                  <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} to="/manage-database"><CloudDownload /> Database</NavDropdown.Item>
-                </NavDropdown>]
-            ) : ''}
+            <Nav.Link id={COMPONENT_IDS.NAVBAR_ABOUT_US} as={NavLink} to="/aboutus" key="about-us">About Us</Nav.Link>
+            <NavDropdown title="Opportunities" id="volunteering-dropdown">
+              <NavDropdown.Item as={NavLink} to="/volunteer-opportunities">Volunteering Opportunities</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/volunteer-organizations"> Volunteering Organizations </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link id={COMPONENT_IDS.NAVBAR_PRICING} as={NavLink} to="/pricing" key="pricing">Pricing</Nav.Link>
+            <Nav.Link id={COMPONENT_IDS.NAVBAR_FAQ} as={NavLink} to="/faq" key="faq">FAQ</Nav.Link>
           </Nav>
           <Nav className="justify-content-end">
             {currentUser === '' ? (
@@ -40,6 +45,8 @@ const NavBar = () => {
               </NavDropdown>
             ) : (
               <NavDropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} title={currentUser}>
+                <Nav.Link id={COMPONENT_IDS.NAVBAR_MY_PROFILE} as={NavLink} to="/profile/:_id" key="profile">View My Profile</Nav.Link>,
+                <Nav.Link id={COMPONENT_IDS.NAVBAR_MY_REPORTS} as={NavLink} to="/reports/:_id" key="report">My Reports</Nav.Link>,
                 <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} as={NavLink} to="/signout"><BoxArrowRight /> Sign out</NavDropdown.Item>
               </NavDropdown>
             )}
