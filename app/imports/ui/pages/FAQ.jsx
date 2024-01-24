e1946c4a39fe58f4494f7cd83ffb409320e22f64
tylerz1443 <42786943+tylerz1443@users.noreply.github.com>
2024-01-24 10:55:46 -1000

    Update FAQ.jsx

--- app/imports/ui/pages/FAQ.jsx
+++ app/imports/ui/pages/FAQ.jsx
@@ -10,17 +10,14 @@ const FAQ = () => (
       <Col xs={8} className="d-flex flex-column justify-content-center">
         <h1>FAQ</h1>
         <ol>
-
           <li><strong>How will we Amplify Impact? (volunteers)</strong>
             Our platform enhances the volunteer experience by addressing the core reasons individuals choose to
             volunteer
           </li>
-
           <li><strong>How will we Amplify Impact? (nonprofits)</strong>
             Our platform directly addresses the biggest challenge faced by volunteer managers: Volunteer Recruitment*
             Our platform also indirectly addresses many of the top goals for volunteer managers.
           </li>
-
         </ol>
       </Col>
 
