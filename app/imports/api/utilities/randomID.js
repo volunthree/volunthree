4ae5fc31c41cc6cb98e966420697deb2a1dd125d
kairemUH <kairem@hawaii.edu>
2024-02-07 08:03:55 -1000

    Added export

--- app/imports/api/utilities/randomID.js
+++ app/imports/api/utilities/randomID.js
@@ -1,3 +1,3 @@
 /** Generate a random ID 19 characters long */
 
-const randomUID = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
+export const randomUID = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
