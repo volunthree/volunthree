1172b300c27426fc55e0f67bbc9991d2b9e6712e
kairemUH <kairem@hawaii.edu>
2024-02-07 07:58:15 -1000

    Moved into directory

--- app/imports/api/organizations/Organizations.js
+++ app/imports/api/organizations/Organizations.js
@@ -1,6 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
-import randomUID from 'utilities/randomID';
+import randomUID from 'app/imports/api/utilities/randomID';
 
 /**
  * The OrganizationCollection. It is the collection of non-profit or volunteer organizations.
