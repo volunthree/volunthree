7e9c5b2a9ab12198368e4bc8587ad4e36c6283c4
Kenji Sanehira <kenjisan@hawaii.edu>
2024-02-28 11:53:25 -1000

    Events Calendar now populated.
    Need to figure out why months are  +1 as well as routing to events.

--- app/imports/ui/components/BigEventCalendar.jsx
+++ app/imports/ui/components/BigEventCalendar.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+// import { useNavigate } from 'react-router-dom';
+import { tempOpportunities } from '../utilities/LocalVariables';
 
 const localizer = momentLocalizer(moment);
+/**
+const handleSelectEvent =
+  (event) => {
+    const navigate = useNavigate();
+    navigate(`/volunteer-event/${event._id}`);
+  };
+ */
 
 // TODO: Once the volunteer collection is initalized, we can use the data from the collection to populate the calendar.
 const BigEventCalendar = () => (
   <div style={{ height: 700 }}>
     <Calendar
       localizer={localizer}
-      startAccessor="start"
-      endAccessor="end"
+      events={tempOpportunities}
+      titleAccessor="name"
+      startAccessor="opportunityDate"
+      endAccessor="opportunityDate"
+      // onSelectEvent={handleSelectEvent}
       style={{ borderRadius: '5px' }}
     />
   </div>
