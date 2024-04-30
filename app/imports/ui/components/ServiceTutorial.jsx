83fa86d48b64d1515f51d19820d5eb0e720d9c5d
Kenji Sanehira <kenjisan@hawaii.edu>
2024-04-30 11:13:39 -1000

    Initial Rework of Cards and View for Page

---
+++ app/imports/ui/components/ServiceTutorial.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const ServiceTutorial = () => (
+  <Container fluid>
+    <Container className="d-flex flex-wrap justify-content-center">
+      <h1 className="mt-4">Tutorial Here</h1>
+    </Container>
+  </Container>
+
+);
+
+export default ServiceTutorial;
