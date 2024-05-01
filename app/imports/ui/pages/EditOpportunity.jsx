7173091785f853fde597dc4c2c61edff03c9ddf0
Tevin Takata <takatat9@hawaii.edu>
2024-05-01 04:17:43 -1000

    Updated layout of all forms
    (MakeARequest, AddOpportunity, EditOpportunity, EditProfile, SignUp) to include multiple columns

--- app/imports/ui/pages/EditOpportunity.jsx
+++ app/imports/ui/pages/EditOpportunity.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { tempOrganizations } from '../utilities/LocalVariables';
 
@@ -40,42 +40,78 @@ const EditOpportunity = () => {
       </Container>
       <Container style={{ padding: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)', marginTop: '30px' }}>
         <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formName">
-            <Form.Label>Opportunity Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter the name of the opportunity."
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formEmail">
-            <Form.Label>Opportunity Organization</Form.Label>
-            <Form.Select value={organization} onSelect={(e) => setOrganization(e.target)}>
-              <option>Select the opportunity organization:</option>
-              {tempOrganizations.map((org) => (
-                <option> {org.organizationName} </option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formImage">
-            <Form.Label>Opportunity Image</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Select a picture that best fits the opportunity."
-              value={image}
-              onChange={(e) => setImage(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formLocation">
-            <Form.Label>Location</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter the location of the opportunity"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            />
-          </Form.Group>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Opportunity Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter the name of the opportunity."
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Opportunity Organization</Form.Label>
+                <Form.Select value={organization} onSelect={(e) => setOrganization(e.target)}>
+                  <option>Select the opportunity organization:</option>
+                  {tempOrganizations.map((org) => (
+                    <option> {org.organizationName} </option>
+                  ))}
+                </Form.Select>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="formDate">
+                <Form.Label>Opportunity Date</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter the opportunity date."
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3" controlId="formLocation">
+                <Form.Label>Location</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter the location of the opportunity"
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="formSkills">
+                <Form.Label>Skills Required</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter useful opportunity skills"
+                  value={skills}
+                  onChange={(e) => setSkills(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3" controlId="formImage">
+                <Form.Label>Opportunity Image</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Select a picture that best fits the opportunity."
+                  value={image}
+                  onChange={(e) => setImage(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
           <Form.Group className="mb-3" controlId="formDescription">
             <Form.Label>Description</Form.Label>
             <Form.Control
@@ -83,24 +119,6 @@ const EditOpportunity = () => {
               placeholder="Enter the opportunity Description"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formDate">
-            <Form.Label>Opportunity Date</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter the opportunity date."
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formSkills">
-            <Form.Label>Skills Required</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter useful opportunity skills"
-              value={skills}
-              onChange={(e) => setSkills(e.target.value)}
             />
           </Form.Group>
           <Button variant="success" type="submit" as={NavLink} to="/volunteer-opportunities">
